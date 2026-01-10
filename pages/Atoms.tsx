511929bd50e99eed2edf5d365a5718e43be2e88d
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2026-01-11 02:31:04 +0700

    update atoms example component

--- pages/Atoms.tsx
+++ pages/Atoms.tsx
@@ -9,13 +9,35 @@ import { Bookmark, Heart, MessageCircle, Send } from 'lucide-react';
 const Atoms: React.FC = () => {
   return (
     <MainLayout>
-      <div className="p-4 flex flex-col gap-4">
-        <Avatar
-          src="https://res.cloudinary.com/dreday4jy/image/upload/v1768069662/Inner_Oval_3_p2qrso.png"
-          hasStory={true}
-          isLive={false}
-        />
-        <Username name="inzoid" isVerified={true} />
+      <div className="p-4 flex flex-col gap-3">
+        <div className="flex gap-6">
+          <div className="flex flex-col items-center gap-2">
+            <Avatar
+              src="https://www.transparentpng.com/thumb/user/gray-user-profile-icon-png-fP8Q1P.png"
+              hasStory={true}
+              isLive={false}
+            />
+            <Username name="User" isVerified={false} />
+          </div>
+
+          <div className="flex flex-col items-center gap-2">
+            <Avatar
+              src="https://res.cloudinary.com/dreday4jy/image/upload/v1768069662/Inner_Oval_3_p2qrso.png"
+              hasStory={false}
+              isLive={false}
+            />
+            <Username name="inzoid" isVerified={true} />
+          </div>
+
+          <div className="flex flex-col items-center gap-2">
+            <Avatar
+              src="https://res.cloudinary.com/dreday4jy/image/upload/v1768069076/Inner_Oval_tnwbky.png"
+              hasStory={true}
+              isLive={true}
+            />
+            <Username name="Sarah" isVerified={false} />
+          </div>
+        </div>
 
         <PostImage src="https://res.cloudinary.com/dreday4jy/image/upload/v1768069076/Rectangle_cml9cz.png" />
       </div>
